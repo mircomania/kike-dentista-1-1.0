4f4e87d9d441ca474a7d0a3c0a205f4f619f857b
mircomania <mirco.rodriguez@hotmail.com>
2026-04-14 14:51:32 -0400

    12 commit - kike-dentista-1-1.0 - correcciones dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="kike-dentista-1-1.0">
+    <BrowserRouter>
         <HelmetProvider>
             <App />
         </HelmetProvider>
